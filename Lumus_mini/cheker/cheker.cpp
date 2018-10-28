05f844e1e9a2842ccb35d59916dfc9b3f92f2b1a
381706LembrikovStepan <stepan.lembrikov.ru@mail.ru>
2018-10-28 23:25:09 +0300

    пара фиксов

--- Lumus_mini/cheker/cheker.cpp
+++ Lumus_mini/cheker/cheker.cpp
@@ -8,24 +8,36 @@ using namespace std;
 void main()
 {
 	setlocale(LC_ALL, "russian");
-	FILE *f = 0;
+	FILE *vozr = 0;
+	FILE *ubiv = 0;
 	FILE *l = 0;
 	int i;
 	int *A;
 	int razmer;
 	int a = 0;
-	f = fopen("..\\Lumus_mini\\vixodnie_dannie.txt", "r");
+	vozr = fopen("..\\Lumus_mini\\Vozr.txt", "r");
+	ubiv = fopen("..\\Lumus_mini\\Ubiv.txt", "r");
 	l = fopen("..\\Cheker\\pravilnost_programmi.txt", "w");
-	fscanf(f, "%d \n", &razmer);
+	fscanf(vozr, "%d \n", &razmer);
 	A = new int[razmer];
 	for (i = 0; i < razmer; i++)
-		fscanf(f, "%d \t", &A[i]);
+		fscanf(vozr, "%d \t", &A[i]);
 	for (i = 0; i < razmer - 1; i++)
 		if (A[i] > A[i + 1])
 			a = 1;
 	if (a == 1)
-		fprintf(l, "программа работает не верно");
+		fprintf(l, "программа сортирует массив по возрастанию не верно\n");
 	else
-		fprintf(l, "всё верно");
-	cout << A[0];
+		fprintf(l, "программа сортирует массив по возрастанию верно\n");
+	a = 0;
+	fscanf(ubiv, "%d \n", &razmer);
+	for (i = 0; i < razmer; i++)
+		fscanf(ubiv, "%d \t", &A[i]);
+	for (i = 0; i < razmer - 1; i++)
+		if (A[i] < A[i + 1])
+			a = 1;
+	if (a == 1)
+		fprintf(l, "программа сортирует массив по убыванию не верно\n");
+	else
+		fprintf(l, "программа сортирует массив по убыванию верно\n");
 }
